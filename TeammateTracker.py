7e159d082e00cfc44a2a72556a22154e0020e5cd
Block011 <Block011@cougars.csusm.edu>
2018-10-23 11:14:42 -0700

    Using schedule to run every 5 seconds. Using psutil to check if process in running.

--- TeammateTracker.py
+++ TeammateTracker.py
@@ -1,17 +1,47 @@
 #imports
+import psutil
+import schedule
+import time
+
 from userClass import userClass
 
 
+#LeagueClientUxRender
+def isProcessRunning(processName):
+	if (processName in (p.name() for p in psutil.process_iter())):
+		return True
+	return False
 
+def checkProcess(user, processName):
+
+	if(isProcessRunning(processName)):
+		print("FoundProcess")
+		#response = user.getParticipants
+		#for user in response:
+		#	user.pushToJSON(response[user])
+	else:
+		print("didNotFindProcess")
+
+def test():
+	print("Test")
 
 def main():
+	processName = "League of Legends.exe"
+	
 	region = "na1"
 	userName = "us economy"
 	apiKey = "RGAPI-343570c7-c17c-4537-939d-9f2e6f34a6f3"
-	user = userClass(region, userName, apiKey)
-	players = user.getParticipants()
 
-	user.pushtoJSON(players)
+	user = userClass(region, userName, apiKey)
+	#players = user.getParticipants()
+
+	schedule.every(5).seconds.do(checkProcess, user, processName)
+
+	while (True):
+		schedule.run_pending()
+		time.sleep(5)
+
+	#user.pushtoJSON(players)
 
 
 
